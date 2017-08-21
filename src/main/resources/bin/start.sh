f9651341901eb3f43aabf6a3e5b4642bd3fbe669
amotov <alexandr.motov@kaspibank.kz>
2017-08-21 20:59:02 +0600

    Fixed logging system.

--- src/main/resources/bin/start.sh
+++ src/main/resources/bin/start.sh
@@ -1,6 +1,19 @@
 ENV=$1
-if [ $ENV == '' ]
-then
-$ENV = "dev";
+if [ "$ENV" == "" ]; then
+    ENV="dev";
 fi
-java -Xms256m -Xmx512m -server -Xloggc:../logs/gc.log -verbose:gc -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=../logs -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9888 -Dcom.sun.management.jmxremote.ssl=FALSE -Dcom.sun.management.jmxremote.authenticate=FALSE -Dlogging.config=../config/log4j2.xml -Dspring.config.location=../config/application.properties -jar ../lib/file-manager-0.1.jar
+
+java -Xms256m \
+     -Xmx512m \
+     -server \
+     -Xloggc:../logs/gc.log -verbose:gc \
+     -XX:+PrintGCDetails \
+     -XX:+HeapDumpOnOutOfMemoryError \
+     -XX:HeapDumpPath=../logs \
+     -Dcom.sun.management.jmxremote \
+     -Dcom.sun.management.jmxremote.port=9888 \
+     -Dcom.sun.management.jmxremote.ssl=FALSE \
+     -Dcom.sun.management.jmxremote.authenticate=FALSE \
+     -Dlogging.config=../config/logback-spring.xml \
+     -Dspring.config.location=../config/application.properties \
+     -jar ../lib/file-manager-0.1.jar
