00f63613e50b078c40f58813d5627707dd8aec17
amotov <alexandr.motov@kaspibank.kz>
2018-02-07 00:39:53 +0600

    Fixed script (again).

--- src/main/resources/bin/start.sh
+++ src/main/resources/bin/start.sh
@@ -7,14 +7,14 @@ fi
 java -Xms256m \
      -Xmx1024m \
      -server \
-     -Xloggc:../logs/gc.log -verbose:gc \
+     -Xloggc:$DIRECTORY/../logs/gc.log -verbose:gc \
      -XX:+PrintGCDetails \
      -XX:+HeapDumpOnOutOfMemoryError \
-     -XX:HeapDumpPath=../logs \
+     -XX:HeapDumpPath=$DIRECTORY../logs \
      -Dcom.sun.management.jmxremote \
      -Dcom.sun.management.jmxremote.port=9888 \
      -Dcom.sun.management.jmxremote.ssl=FALSE \
      -Dcom.sun.management.jmxremote.authenticate=FALSE \
-     -Dlogging.config=../config/logback-spring.xml \
-     -Dspring.config.location=../config/application.properties \
+     -Dlogging.config=$DIRECTORY/../config/logback-spring.xml \
+     -Dspring.config.location=$DIRECTORY/../config/application.properties \
      -jar $DIRECTORY/../lib/file-manager-0.1.jar
